11dad60b98edfdd780e127a3a31255838358040b
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-10-26 13:10:08 -0500

    update project

--- proyecto/Owner.sql
+++ proyecto/Owner.sql
@@ -16,3 +16,9 @@ CREATE TABLE hoteles(
     CIUDAD     VARCHAR(50)     NOT NULL,
     CONSTRAINT hoteles_pk PRIMARY KEY(ID)
 );
+
+/* 2.2 ingresar datos a la Tabla hoteles */
+INSERT INTO hoteles (ID, DIRECCION, CIUDAD) VALUES (1, 'Carrera 1A', 'Popayán')
+INSERT INTO hoteles (ID, DIRECCION, CIUDAD) VALUES (2, 'Carrera 1B', 'Cali')
+INSERT INTO hoteles (ID, DIRECCION, CIUDAD) VALUES (3, 'Carrera 2A', 'Medellin')
+INSERT INTO hoteles (ID, DIRECCION, CIUDAD) VALUES (4, 'Carrera 7A', 'Bogota')
