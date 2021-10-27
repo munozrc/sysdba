5678a13644e03f76dc0033461dcc28f1cb0904d4
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-10-27 11:46:21 -0500

    añadir permisos para crear vistas

--- proyecto/DBA.sql
+++ proyecto/DBA.sql
@@ -10,39 +10,41 @@
     - Crear usuario Audit: 1.7-1.11
 */
 
-/* 1.1 consultamos los tablespace disponibles en la DB mediante el comando */
+-- 1.1 consultamos los tablespace disponibles en la DB mediante el comando
 SELECT TABLESPACE_NAME FROM dba_tablespaces;
 
-/* 1.2 creamos el usuario OWNER */
+-- 1.2 creamos el usuario OWNER
 CREATE USER Owner IDENTIFIED BY 123456;
 
-/* 1.3 asignamos los permisos para crear una sesion */
+-- 1.3 asignamos los permisos para crear una sesion
 GRANT CREATE SESSION TO Owner;
 
-/* 1.4 asignamos el usuario a tablespace USERS */
+-- 1.4 asignamos el usuario a tablespace USERS
 ALTER USER Owner DEFAULT TABLESPACE USERS;
 
-/* 1.5 determinamos el espacio sobre el tablespace USERS */
+-- 1.5 determinamos el espacio sobre el tablespace USERS
 ALTER USER Owner QUOTA UNLIMITED ON USERS;
 
-/* 1.6 asignamos el permiso para crear tablas, secuencias y triggers */
+-- 1.6 asignamos el permiso para crear tablas, secuencias, vistas y triggers
 GRANT CREATE TABLE TO Owner;
 GRANT CREATE SEQUENCE TO Owner;
 GRANT CREATE TRIGGER TO Owner;
+GRANT CREATE VIEW TO Owner;
 
-/* 1.7 creamos el usuario para las auditorias */
+-- 1.7 creamos el usuario para las auditorias
 CREATE USER Audit IDENTIFIED BY 123456;
 
-/* 1.8 asignamos los permisos para crear una sesion */
+-- 1.8 asignamos los permisos para crear una sesion
 GRANT CREATE SESSION TO Audit;
 
-/* 1.9 asignamos el usuario a tablespace USERS */
+-- 1.9 asignamos el usuario a tablespace USERS
 ALTER USER Audit DEFAULT TABLESPACE USERS;
 
-/* 1.10 determinamos el espacio sobre el tablespace USERS */
+-- 1.10 determinamos el espacio sobre el tablespace USERS
 ALTER USER Audit QUOTA UNLIMITED ON USERS;
 
-/* 1.6 asignamos el permiso para crear tablas, secuencias y triggers */
+-- 1.6 asignamos el permiso para crear tablas, secuencias, vistas  y triggers
 GRANT CREATE TABLE TO Audit;
 GRANT CREATE SEQUENCE TO Audit;
 GRANT CREATE TRIGGER TO Audit;
+GRANT CREATE VIEW TO Audit;
